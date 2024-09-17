f18b0e18099098d354218cc7ac9be063f66240c0
kylieMarxen <771874@calbaptist.edu>
2024-09-16 23:06:25 -0700

    Triangles code along

---
+++ src/Triangles.java
@@ -0,0 +1,26 @@
+import java.util.*; //so I can use scanner
+
+public class Triangles {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Triangle size?");
+        int triangleSize = console.nextInt();
+        System.out.print("Fill character?");
+        String fill = console.next();
+        drawTriangle(triangleSize, fill);
+    }
+
+    public static void drawTriangle(int size, String fill) {
+        for (int line = 1; line <= size; line++) {
+            printString(" ", -1 * line + size);
+            printString(fill, 2 * line -1);
+            System.out.println();
+        }
+    }
+
+    public static void printString(String s, int times) {
+        for (int i = 1; i <= times; i++) {
+            System.out.print(s);
+        }
+    }
+}
