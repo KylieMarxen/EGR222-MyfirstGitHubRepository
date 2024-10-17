94ae3dfaffeee6e45fe670b4a4671ba8f35c9a17
kylieMarxen <771874@calbaptist.edu>
2024-10-16 17:15:50 -0700

    code along 8.3 advanced methods

--- src/Date.java
+++ src/Date.java
@@ -42,10 +42,57 @@ public class Date {
             return 365;
         }
     }
+    //accessor- return information about state of the object
+    //mutator - changes the state of the object
 
+    //returns true if this date occurs in a leap year
     public boolean isLeapYear(){
         return ( year % 400 == 0  || (year % 4 == 0 && year % 100 != 0 ));
 
+        }
+        public void nextDay(){
+        // 1. within same month
+            // 2. wrapping to next month
+            //3. wrapping to next year
+        day++;
+        if (day > getDaysInMonth()) {
+            //wrapping to next month
+            month++;
+            day = 1;
+            if(month > 12){
+                //wrapping to next year
+                month = 1;
+            }
+
+        }
+
+        }
+        public int daysBetween(Date other){
+        // count number of days
+            // start at this date, move forward until we reach other
+            int count = 0;
+
+            //assumes this date comes before the other date
+            if(before(other)){
+                Date temp = new Date(this);
+                while (temp.year != other.year || temp.month != other.month || temp.day != other.day) {
+                    temp.nextDay(); //side effect
+                    count++;
+                }
+            }else{
+                // this date is >= other date
+                Date temp = new Date(other);
+                while (temp.year != year || temp.month != month || temp.day != day) {
+                    temp.nextDay(); //side effect
+                    count++;
+                }
+            }
+            return count;
+        }
+        public boolean before (Date other){
+        return (year < other.year ||
+                (year == other.year && (month < other.month ||
+                        month == other.month && day < other.day)));
         }
     }
 
--- src/DateMain.java
+++ src/DateMain.java
@@ -1,21 +1,38 @@
 // this client program uses the Date class
 public class DateMain {
     public static void main(String[] args) {
-        Date d1 = new Date(2009,10,17);
-         Date d2 = new Date(2008,2,3);
+        Date d1 = new Date(2008,3,1);
+         Date d2 = new Date(2008,5,10);
          Date d3 = new Date(d1);
+         Date d4 = new Date(2010, 12, 31);
 
          System.out.println(d1.year);
          System.out.println(d1.getDaysInMonth());
         System.out.println(d1.getDaysInYear());
         System.out.println(d1.isLeapYear());
+        System.out.println(d1.year + "/" + d1.month + "/" + d1.day);
+        d1.nextDay(); //10/18
+        System.out.println(d1.year + "/" + d1.month + "/" + d1.day);
         System.out.println(d2.getDaysInMonth());
         System.out.println(d2.getDaysInYear());
         System.out.println(d2.isLeapYear());
+        System.out.println(d1.year + "/" + d2.month + "/" + d2.day);
+        d2.nextDay(); //10/18
+        System.out.println(d1.year + "/" + d2.month + "/" + d2.day);
         System.out.println(d3.year);
         System.out.println(d3.getDaysInMonth());
         System.out.println(d3.getDaysInYear());
         System.out.println(d3.isLeapYear());
+
+        System.out.println(d4.year + "/" + d4.month + "/" + d4.day);
+        d4.nextDay(); //10/18
+        System.out.println(d4.year + "/" + d4.month + "/" + d4.day);
+
+        System.out.println(d1.daysBetween(d2));
+        System.out.println(d1.daysBetween(d2));
+       System.out.println(d2.daysBetween(d1));
+       //System.out.println(d1.daysBetween(d1));
+
     }
 
 }
