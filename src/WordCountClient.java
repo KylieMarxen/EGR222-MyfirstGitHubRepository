2433350b71171ea8d7ec7843c398d859cda841da
kylieMarxen <771874@calbaptist.edu>
2024-11-13 16:13:15 -0800

    Set collection code along

---
+++ src/WordCountClient.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class WordCountClient {
+    public static void main(String[] args) throws FileNotFoundException {
+            // Create an instance of the WordCount class
+            WordCount wordCount = new WordCount();
+
+            wordCount.main(args);
+
+
+    }
+}
